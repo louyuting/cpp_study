da790b670a0a33b7d7d4c4e1d1178606c2657b72
louyuting <1849491904@qq.com>
2019-02-27 23:08:13 +0800

    restruct

---
+++ src/main/study/test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 娄宇庭 on 2019/2/27.
+//
+#include <iostream>
+
+int main(){
+    int a = 88;
+    int b = a;
+    b = 99;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+}
